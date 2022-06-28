c6cde37fd400301cc4ea0e6e03c65b43c1a2bad4
dineshdk2512 <dineshdanish46@gmail.com>
2022-06-28 21:40:15 +0530

    First time push

---
+++ src/test/java/com/sample/Class1.java
@@ -0,0 +1,17 @@
+package com.sample;
+
+public class Class1 {
+
+	private void m1() {
+		System.out.println("method 1");
+
+	} 
+	private void m2() {
+		System.out.println("method 2");
+
+	}
+	private void m3() {
+		System.out.println("method 3");
+
+	}
+}
